252362ce8bc6b65010ad5e5a2080bc982b0338ee
Henrik Vendelbo <henrik@thepia.com>
2015-12-06 21:05:32 +0100

    client build

--- salespartner-angular/client/gulpfile.js
+++ salespartner-angular/client/gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    path = require('path'),
     browserify = require('browserify'),
     jshint = require('gulp-jshint'),
     htmlhint = require('gulp-htmlhint'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
     tap = require('gulp-tap'),
     plumber = require('gulp-plumber'),
     connect = require('gulp-connect');
+
 
 gulp.task('dist', ['html', 'scripts', 'styles']);
 
@@ -24,15 +33,50 @@ gulp.task('html', function() {
     .pipe(htmlhint.failReporter());
 });
 
+var ngHtml2Js = require('browserify-ng-html2js')({
+    //stripPrefix: resourceRoot + "/",
+    extension: 'tpl.html'
+});
+
 gulp.task('scripts', function() {
   gulp.src('./app/app.js')
     .pipe(plumber())
-    .pipe(tap(function(file) {
-      
-    }))
-
+    .pipe(tap(
+        function(file) {
+            var d = require('domain').create();
+            d.on("error", function(err) {
+                gutil.log(gutil.colors.red('Browserify compile error:'),
+                    err.message,
+                    '\n\t', gutil.colors.cyan('in file'), file.path);
+            });
+            d.run(function() {
+                file.contents = browserify({
+                    entries: file.path,
+                    debug: true,
+                    // defining transforms here will avoid crashing your stream
+                    transform: [ngHtml2Js, 'debowerify', 'browserify-ngannotate']
+                }).bundle();
+            });
+        }
+        ))
+    .pipe(buffer())
+    .pipe(gulp.dest('./assets'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./assets'));
 });
 
 gulp.task('styles', function() {
-
+    gulp.src('./assets/all.scss')
+        .pipe(plumber())
+        .pipe(sass({
+            sourcemap: true,
+            includePaths: ['./../styleguide/styles',path.join(__dirname,'..'),'bower_components']
+        }))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(minifycss())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./assets'));
 });
