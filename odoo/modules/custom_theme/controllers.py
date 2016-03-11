eecf463dbda6393867679acf0bc1005b9f2f3e2f
Ilya Khalturinskiy <khalturinsky@gmail.com>
2016-03-11 17:11:42 +0300

    delete old modules #19

--- odoo/modules/custom_theme/controllers.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import http
-
-class Academy(http.Controller):
-    @http.route('/academy/', auth='public')
-    def index(self):
-        return "Hello, world!"
-    
